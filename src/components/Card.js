4ca702486b56e0549f266a44807361c1eca1c651
Takao Mizuno <takaomizuno@takaoMacBook-Air.local>
2022-11-08 20:26:38 +1000

    create prototype

---
+++ src/components/Card.js
@@ -0,0 +1,109 @@
+import * as React from 'react';
+import styled from '@mui/material/styles/styled';
+import PropTypes from 'prop-types';
+import Paper from '@mui/material/Paper';
+import { Typography, Grid, Box, keyframes } from '@mui/material';
+import { TypeAnimation } from 'react-type-animation';
+
+
+function Card(props) {
+    const { post } = props;
+
+    const trackingInContract = keyframes`
+        0% {
+            letter-spacing: 1em;
+            opacity: 0;
+        }
+        40% {
+            opacity: 0.6;
+        }
+        100% {
+            letter-spacing: normal;
+            opacity: 1;
+        }
+    `;
+
+    const AutName = styled(Typography)(() => ({
+        animation: `0.8s ${trackingInContract} 1.2s cubic-bezier(0.215, 0.610, 0.355, 1.000) both`
+
+    }));
+
+    // const Description = styled(Typography)(() => ({
+    //     animation: `${popDown} 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both`
+    // }))
+
+
+    return (
+        <Paper
+            sx={{
+                position: 'relative',
+                backgroundColor: 'grey.800',
+                color: '#fff',
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center',
+                backgroundImage: `url(${post.image})`,
+                transparent: 0.5,
+                minHeight: '100vh'
+            }}
+        >
+            {/* Increase the priority of the hero background image */}
+            {<img style={{ display: 'none' }} src={post.image} alt={post.imageText} />}
+            <Box
+                sx={{
+                    position: 'absolute',
+                    top: 0,
+                    bottom: 0,
+                    right: 0,
+                    left: 0,
+                    backgroundColor: 'rgba(0,0,0,.3)',
+                }}
+            />
+
+            <Grid container sx={{
+                height: '250px',
+                top: '50%',
+                marginTop: '-125px',
+                position: 'absolute'
+            }}>
+                <Grid item xs={12} mg={12} lg={12}>
+                    <Box
+                        sx={{
+                            position: 'relative',
+                            p: { xs: 3, md: 6 },
+                            pr: { md: 0 },
+                        }}
+                    >
+                        <AutName component="h1" variant="h3" color="inherit" gutterBottom sx={{ fontFamily: 'Robot' }}>
+                            {post.title}
+                        </AutName>
+                        <TypeAnimation
+                            sequence={[
+                                1300,
+                                post.description,
+                                1000,
+                                post.descriptionTwo,
+                                1000,
+                                ''
+                            ]}
+                            cursor={true}
+                            repeat={Infinity}
+                        />
+
+                    </Box>
+                </Grid>
+            </Grid>
+        </Paper >
+    );
+}
+
+Card.propTypes = {
+    post: PropTypes.shape({
+        description: PropTypes.string.isRequired,
+        image: PropTypes.string.isRequired,
+        imageText: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+    }).isRequired,
+};
+
+export default Card;
---
+++ src/components/Skills.js
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import { Grid, Typography } from '@mui/material';
+import Skill from '../components/Skill';
+import CIcon from '../static/img/c_icon.png';
+import PythonIcon from '../static/img/python_icon.png';
+import TypescriptIcon from '../static/img/typescript_icon.png';
+import ReactIcon from '../static/img/react_icon.jpg';
+
+
+function Skills(props) {
+    const skill1 = {
+        language: "C#",
+        description: "I have experienced C# for 3 years at an IT company in Japan. This is my fundamental skill.",
+        image: CIcon,
+        imageLabel: "c#-icon"
+    }
+
+    const skill2 = {
+        language: "Python",
+        description: "I write scripts by python to make my life be more convenient. In addition, I solve some algorithm problems by python.",
+        image: PythonIcon,
+        imageLabel: "python-icon"
+    }
+
+    const skill3 = {
+        language: "Typescript",
+        description: "In Japan, I created UI by typescript and Knockout.js framework. This is my front-end skill.",
+        image: TypescriptIcon,
+        imageLabel: "typescript-icon"
+    }
+
+    const skill4 = {
+        language: "React",
+        description: "This site is created by React and Material UI. This is new skill I am learning. ",
+        image: ReactIcon,
+        imageLabel: "react-icon"
+    }
+
+    const skills = [
+        skill1, skill2, skill3, skill4
+    ]
+
+    return (
+        <Grid container>
+            <Grid item xs={12} md={12} lg={12} sx={{ background: 'black' }}>
+                <Typography variant='h4' align='center' color={'white'} gutterBottom >
+                    Skills
+                </Typography>
+            </Grid>
+            {
+                skills.map((skill, i) => (
+                    <Skill key={i} skill={skill} sx={{ marginBottom: '3px' }} />
+                ))}
+
+
+        </Grid>
+    );
+}
+
+export default Skills;
