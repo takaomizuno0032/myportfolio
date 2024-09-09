475335af0d18b23adf66492f43687f9c51858632
Takao Mizuno <waterhawk0032@gmail.com>
2024-09-10 09:41:12 +1000

    chainge Header's button design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,132 +12,140 @@ import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 
 function Header() {
-  const navigate = useNavigate();
-  const [anchorElNav, setAnchorElNav] = useState(null);
+    const navigate = useNavigate();
+    const [anchorElNav, setAnchorElNav] = useState(null);
 
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
 
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
 
-  const pages = ["home", "personal", "skills", "portfolios"];
+    const pages = ["home", "personal", "skills", "portfolios"];
 
-  const onClick = (page) => {
-    if (page === "home") {
-      navigate("/myportfolio");
-    } else {
-      navigate(`/myportfolio/${page}`);
-    }
-  };
+    const onClick = (page) => {
+        if (page === "home") {
+            navigate("/myportfolio");
+        } else {
+            navigate(`/myportfolio/${page}`);
+        }
+    };
 
-  return (
-    <AppBar position="static" color="custom">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="/myportfolio"
-            sx={{
-              mr: 2,
-              display: { xs: "none", md: "flex" },
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "white",
-              textDecoration: "none",
-            }}
-          >
-            Takao Mizuno
-          </Typography>
+    return (
+        <AppBar position="static" color="custom">
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        href="/myportfolio"
+                        sx={{
+                            mr: 2,
+                            display: { xs: "none", md: "flex" },
+                            fontFamily: "monospace",
+                            fontWeight: 700,
+                            letterSpacing: ".3rem",
+                            color: "white",
+                            textDecoration: "none",
+                        }}
+                    >
+                        Takao Mizuno
+                    </Typography>
 
-          <Box
-            sx={{
-              flexGrow: 1,
-              display: { xs: "flex", md: "none", color: "white" },
-            }}
-          >
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: "block", md: "none" },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem onClick={handleCloseNavMenu}>
-                  <Typography
-                    key={page}
-                    onClick={() => {
-                      onClick(page);
-                    }}
-                  >
-                    {page.toUpperCase()}
-                  </Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href="/myportfolio"
-            sx={{
-              mr: 2,
-              display: { xs: "flex", md: "none" },
-              flexGrow: 1,
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "white",
-              textDecoration: "none",
-            }}
-          >
-            Takao Mizuno
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={() => {
-                  onClick(page);
-                }}
-                className="custom-button"
-                sx={{ marginX: "10px" }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
-  );
+                    <Box
+                        sx={{
+                            flexGrow: 1,
+                            display: { xs: "flex", md: "none", color: "white" },
+                        }}
+                    >
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: "bottom",
+                                horizontal: "left",
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: "top",
+                                horizontal: "left",
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: "block", md: "none" },
+                            }}
+                        >
+                            {pages.map((page) => (
+                                <MenuItem onClick={handleCloseNavMenu}>
+                                    <Typography
+                                        key={page}
+                                        onClick={() => {
+                                            onClick(page);
+                                        }}
+                                    >
+                                        {page.toUpperCase()}
+                                    </Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                    <Typography
+                        variant="h5"
+                        noWrap
+                        component="a"
+                        href="/myportfolio"
+                        sx={{
+                            mr: 2,
+                            display: { xs: "flex", md: "none" },
+                            flexGrow: 1,
+                            fontFamily: "monospace",
+                            fontWeight: 700,
+                            letterSpacing: ".3rem",
+                            color: "white",
+                            textDecoration: "none",
+                        }}
+                    >
+                        Takao Mizuno
+                    </Typography>
+                    <Box
+                        sx={{
+                            flexGrow: 1,
+                            display: { xs: "none", md: "flex" },
+                        }}
+                    >
+                        {pages.map((page) => (
+                            <Typography
+                                key={page}
+                                onClick={() => {
+                                    onClick(page);
+                                }}
+                                sx={{
+                                    marginX: "10px",
+                                    color: "white",
+                                    cursor: "pointer",
+                                }}
+                            >
+                                {page}
+                            </Typography>
+                        ))}
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
 }
 export default Header;
