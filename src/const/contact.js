1f059a547ab54589e5bb7ea4020044000c88dee6
Takao Mizuno <waterhawk0032@gmail.com>
2024-09-10 11:07:32 +1000

    add notes for contact

--- src/const/contact.js
+++ src/const/contact.js
@@ -3,30 +3,36 @@ import FacebookIcon from "../static/img/facebook.png";
 import QiitaIcon from "../static/img/qiita.png";
 import InstaIcon from "../static/img/instagram.png";
 import TwitterIcon from "../static/img/twitter.png";
+import NoteIcon from "../static/img/note.png";
 
 const linkedIn = {
-  url: "https://www.linkedin.com/in/takao-mizuno-776a04231/",
-  image: LinkedInIcon,
+    url: "https://www.linkedin.com/in/takao-mizuno-776a04231/",
+    image: LinkedInIcon,
+};
+
+const notes = {
+    url: "https://note.com/takao_mizuno1130/",
+    image: NoteIcon,
 };
 
 const facebook = {
-  url: "https://www.facebook.com/takao.mizino",
-  image: FacebookIcon,
+    url: "https://www.facebook.com/takao.mizino",
+    image: FacebookIcon,
 };
 
 const qiita = {
-  url: "https://qiita.com/mizutakawater/",
-  image: QiitaIcon,
+    url: "https://qiita.com/mizutakawater/",
+    image: QiitaIcon,
 };
 
 const instagram = {
-  url: "https://instagram.com/waterbrisbane?igshid=YmMyMTA2M2Y=",
-  image: InstaIcon,
+    url: "https://instagram.com/waterbrisbane?igshid=YmMyMTA2M2Y=",
+    image: InstaIcon,
 };
 
 const twitter = {
-  url: "https://twitter.com/taka_water_32",
-  image: TwitterIcon,
+    url: "https://twitter.com/taka_water_32",
+    image: TwitterIcon,
 };
 
-export const contacts = [linkedIn, twitter, facebook, qiita, instagram];
+export const contacts = [linkedIn, notes, twitter, facebook, qiita, instagram];
